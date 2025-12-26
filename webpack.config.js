327ebfa215c77d61ca423e186609cc5f86ac1ac9
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-12-27 03:16:08 +0400

    build: enable source maps and watch HTML template

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
@@ -29,7 +33,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      }
+      },
     ],
   },
 };
