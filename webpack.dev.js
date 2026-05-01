83ea2c03129f1f57677de3891646e21514692daa
nishadnp <69951135+nishadnp@users.noreply.github.com>
2026-05-02 01:45:58 +0400

    build: split webpack config file
    - Separate development and production configurations.
    - Introduce webpack-merge for config composition.
    - Update package.json scripts to use new config files.

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+// webpack.dev.js 
+
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    open: true,
+    hot: true,
+    watchFiles: ["./src/**/*"],
+  },
+});
---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+// webpack.prod.js
+
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+});
